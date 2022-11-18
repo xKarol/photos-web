d22ce0ffb3fbc5d6b648b18680cda869562da374
xKarol <karolpiskorz8@gmail.com>
2022-11-18 13:58:58 +0100

    Add font

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Quicksand } from "@next/font/google";
+
+const openSans = Quicksand({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${openSans.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
