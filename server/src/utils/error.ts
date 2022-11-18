108552c42637d9fc8dcb8c083c5d9213f4684558
xKarol <karolpiskorz8@gmail.com>
2022-11-18 19:42:43 +0100

    Better format for zod errors

--- server/src/utils/error.ts
+++ server/src/utils/error.ts
@@ -1,4 +1,7 @@
+import { ZodError } from "zod";
+
 function getErrorMessage(error: unknown) {
+  if (error instanceof ZodError) return error.errors[0].message;
   if (error instanceof Error) return error.message;
   return String(error);
 }
