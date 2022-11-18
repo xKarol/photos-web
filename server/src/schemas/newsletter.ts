710bc3bb9bb73036f5f88dad9ee84e861b19a1a2
xKarol <karolpiskorz8@gmail.com>
2022-11-18 19:42:20 +0100

    Add schema better error message

--- server/src/schemas/newsletter.ts
+++ server/src/schemas/newsletter.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const newsletterSubscribeSchema = z.object({
   body: z.object({
-    email: z.string().email(),
+    email: z.string({ required_error: "Email is required." }).email(),
   }),
 });
 
