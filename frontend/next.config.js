6c1df9decc5b50024cb1cb8ee76ee9c66cfd29f8
xKarol <karolpiskorz8@gmail.com>
2022-11-19 17:38:53 +0100

    Next Config: Allow render all images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        hostname: "*",
+      },
+    ],
   },
 };
 
