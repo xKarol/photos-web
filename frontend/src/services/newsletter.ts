f45ca7114afa61e23e2833a6b90d64e6d8b2a8d0
xKarol <karolpiskorz8@gmail.com>
2022-11-20 16:38:03 +0100

    Get server url from .env

--- frontend/src/services/newsletter.ts
+++ frontend/src/services/newsletter.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:4000";
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const newsletterSubscribe = async (email: string) => {
   return axios.post(`${SERVER_URL}/newsletter/subscribe`, { email });
--- frontend/src/services/photos.ts
+++ frontend/src/services/photos.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:4000";
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const getPhotos = async () => {
   return await axios.get(`${SERVER_URL}/photos`);
