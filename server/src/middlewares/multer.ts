1a1d6f0935c37fe50ff0522bdbf372cbc472a59c
xKarol <karolpiskorz8@gmail.com>
2022-11-24 18:33:31 +0100

    Add max file size to upload

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -5,6 +5,7 @@ import { getFileExtension } from "../utils/file";
 const storage = multer.memoryStorage();
 
 const allowedExtensions = ["jpg", "jpeg", "png"];
+const maxSize = 1024 * 1024 * 5; // 5mb
 
 const upload = multer({
   fileFilter: async function (req, file, cb) {
@@ -19,6 +20,7 @@ const upload = multer({
     }
     cb(null, true);
   },
+  limits: { fileSize: maxSize },
   storage,
 });
 
