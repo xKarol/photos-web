dd65ec9fe839b93ffee133d36285e97b91e51301
xKarol <karolpiskorz8@gmail.com>
2022-11-25 19:14:24 +0100

    Moved multer config to config file

---
+++ server/src/config/multer.ts
@@ -0,0 +1,25 @@
+import multer from "multer";
+
+import { getFileExtension } from "../utils/file";
+
+const storage = multer.memoryStorage();
+
+const allowedExtensions = ["jpg", "jpeg", "png"];
+const maxSize = 1024 * 1024 * 5; // 5mb
+
+export const multerConfig: multer.Options = {
+  fileFilter: async function (req, file, cb) {
+    const extension = getFileExtension(file.originalname) || "";
+    if (!allowedExtensions.includes(extension)) {
+      return cb(
+        new Error(
+          `Invalid file extension. Received: ${extension}
+           Expected: ${allowedExtensions.join(", ")}`
+        )
+      );
+    }
+    cb(null, true);
+  },
+  limits: { fileSize: maxSize },
+  storage,
+};
--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,27 +1,7 @@
 import multer from "multer";
 
-import { getFileExtension } from "../utils/file";
+import { multerConfig } from "../config/multer";
 
-const storage = multer.memoryStorage();
-
-const allowedExtensions = ["jpg", "jpeg", "png"];
-const maxSize = 1024 * 1024 * 5; // 5mb
-
-const upload = multer({
-  fileFilter: async function (req, file, cb) {
-    const extension = getFileExtension(file.originalname) || "";
-    if (!allowedExtensions.includes(extension)) {
-      return cb(
-        new Error(
-          `Invalid file extension. Received: ${extension}
-           Expected: ${allowedExtensions.join(", ")}`
-        )
-      );
-    }
-    cb(null, true);
-  },
-  limits: { fileSize: maxSize },
-  storage,
-});
+const upload = multer(multerConfig);
 
 export { upload };
