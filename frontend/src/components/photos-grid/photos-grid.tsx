0211d661ba3f88f3bae2607a300ca60e099b2ec1
xKarol <karolpiskorz8@gmail.com>
2022-11-26 17:39:26 +0100

    Add basic lightbox modal

--- frontend/src/components/photos-grid/photos-grid.tsx
+++ frontend/src/components/photos-grid/photos-grid.tsx
@@ -1,23 +1,27 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Photo from "./photo";
 import { usePhotos } from "../../hooks/usePhotos";
+import Lightbox from "../lightbox";
 
 const Photos = () => {
   const { ref, data, fetchNextPage } = usePhotos();
+  const [open, setOpen] = useState(false);
 
   const photos = data?.pages.map(({ data }) => data).flat(1) || [];
   const half = photos.length / 2;
 
   return (
-    <section className="container mx-auto">
-      <div className="flex space-x-10">
-        <div className="w-full flex flex-col space-y-10">
-          {photos
-            .slice(0, half)
-            .map(({ id, src, alt, width, height, placeholder }: any) => (
-              <Link href={`/photo/${id}`} key={id}>
+    <>
+      <section className="container mx-auto">
+        <div className="flex space-x-10">
+          <div className="w-full flex flex-col space-y-10">
+            {photos
+              .slice(0, half)
+              .map(({ id, src, alt, width, height, placeholder }: any) => (
+                // <Link href={`/photo/${id}`} key={id}>
                 <Photo
+                  onClick={() => setOpen(true)}
                   className="relative"
                   src={src}
                   alt={alt}
@@ -30,15 +34,16 @@ const Photos = () => {
                     height: "auto",
                   }}
                 />
-              </Link>
-            ))}
-        </div>
-        <div className="w-full flex flex-col space-y-10">
-          {photos
-            .slice(half, photos.length)
-            .map(({ id, src, alt, height, width, placeholder }: any) => (
-              <Link href={`/photo/${id}`} key={id}>
+                // </Link>
+              ))}
+          </div>
+          <div className="w-full flex flex-col space-y-10">
+            {photos
+              .slice(half, photos.length)
+              .map(({ id, src, alt, height, width, placeholder }: any) => (
+                // <Link href={`/photo/${id}`} key={id}>
                 <Photo
+                  onClick={() => setOpen(true)}
                   className="relative"
                   src={src}
                   alt={alt}
@@ -51,14 +56,16 @@ const Photos = () => {
                     height: "auto",
                   }}
                 />
-              </Link>
-            ))}
+                // </Link>
+              ))}
+          </div>
         </div>
-      </div>
-      <button onClick={() => fetchNextPage()} ref={ref}>
-        Load More
-      </button>
-    </section>
+        <button onClick={() => fetchNextPage()} ref={ref}>
+          Load More
+        </button>
+      </section>
+      <Lightbox setIsOpen={setOpen} isOpen={open} />
+    </>
   );
 };
 
