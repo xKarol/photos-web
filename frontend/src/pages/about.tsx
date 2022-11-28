ec9795984f9437b078bb797550484f362605f88b
xKarol <karolpiskorz8@gmail.com>
2022-11-28 21:26:24 +0100

    Add about page content

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/header";
 import { Footer } from "../components/footer";
+import Layout from "../components/layout";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,30 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      <h1>About</h1>
+      <Layout as="main">
+        <div className="flex space-x-10">
+          <figure className="flex-1">
+            <img className="outline-dotted w-full h-[400px]" />
+          </figure>
+          <section className="flex-1 flex flex-col">
+            <h1 className="uppercase text-bold text-3xl mb-5">About</h1>
+            <p className="mb-3 tracking-wide font-light text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem sit adipisci officia culpa fugiat, explicabo optio
+              dolorem doloremque possimus consequatur delectus amet suscipit qui
+              nisi quia numquam, earum quam labore ipsam quas. At rem reiciendis
+              sequi ipsa dolor facilis. Voluptates velit ab quas ipsa hic
+              provident voluptatum. Nam, inventore tempore.
+            </p>
+            <p className="mb-3 tracking-wide font-light text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem sit adipisci officia culpa fugiat, explicabo optio
+              dolorem doloremque possimus consequatur delectus amet suscipit qui
+              nisi quia numquam.
+            </p>
+          </section>
+        </div>
+      </Layout>
       <Footer />
     </>
   );
