a327a7a4e933f46437c16bef8bf4b1c44a331f06
xKarol <karolpiskorz8@gmail.com>
2022-11-29 17:29:09 +0100

    Change photos button styles & add spinner on fetching photos

--- frontend/src/components/photos-grid/photos-grid.tsx
+++ frontend/src/components/photos-grid/photos-grid.tsx
@@ -2,22 +2,33 @@ import React, { useState } from "react";
 import { usePhotos } from "../../hooks/usePhotos";
 import Layout from "../layout";
 import Lightbox from "../lightbox";
+import Spinner from "../spinner";
 import PhotosColumns from "./photos-columns";
 
 const Photos = () => {
-  const { ref, data, fetchNextPage, hasNextPage } = usePhotos();
+  const { ref, data, fetchNextPage, hasNextPage, isFetching } = usePhotos();
   const [open, setOpen] = useState(false);
 
   const photos = data?.pages.map(({ data }) => data).flat(1) || [];
 
   return (
     <>
-      <Layout as="section">
+      <Layout as="section" className="flex flex-col">
         <PhotosColumns onClick={() => setOpen(true)} photos={photos} />
         {hasNextPage ? (
-          <button onClick={() => fetchNextPage()} ref={ref}>
-            Load More
-          </button>
+          <div className="mt-[2.5rem] mx-auto">
+            {isFetching ? (
+              <Spinner color="black" />
+            ) : (
+              <button
+                className=" border border-black py-2 px-4 text-sm"
+                onClick={() => fetchNextPage()}
+                ref={ref}
+              >
+                Load More
+              </button>
+            )}
+          </div>
         ) : null}
       </Layout>
 
