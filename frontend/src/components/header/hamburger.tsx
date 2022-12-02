b479ed8a475447fa694d4739b49b6d1bb3c05906
xKarol <karolpiskorz8@gmail.com>
2022-12-02 21:14:57 +0100

    Add hamburger component

---
+++ frontend/src/components/header/hamburger.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import React from "react";
+
+type Props = Omit<React.ComponentPropsWithoutRef<"button">, "children">;
+
+const Hamburger = ({ className, ...props }: Props) => {
+  return (
+    <button
+      className={clsx("flex flex-col space-y-1 w-[30px]", className)}
+      {...props}
+    >
+      <span className="h-[2px] w-full bg-black" />
+      <span className="h-[2px] w-full bg-black" />
+      <span className="h-[2px] w-full bg-black" />
+    </button>
+  );
+};
+
+export default Hamburger;
--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,13 +1,16 @@
-import Link from "next/link";
 import React from "react";
 import Layout from "../layout";
+import Hamburger from "./hamburger";
 import Navbar from "./navbar";
+import { useMedia } from "react-use";
 
 const Header = () => {
+  const isMobile = useMedia("(max-width: 500px)");
+
   return (
-    <Layout as="header" className="my-20 flex justify-between">
+    <Layout as="header" className="my-20 flex justify-between items-center">
       <figure>Logo</figure>
-      <Navbar />
+      {isMobile ? <Hamburger /> : <Navbar />}
     </Layout>
   );
 };
