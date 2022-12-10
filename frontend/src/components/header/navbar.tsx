09a7227e90589c37af49e347fcf595cfd057e168
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-10 20:09:15 +0000

    Add navbar items hover effect

--- frontend/src/components/header/navbar.tsx
+++ frontend/src/components/header/navbar.tsx
@@ -5,15 +5,30 @@ import { ROUTE_ABOUT, ROUTE_CONTACT, ROUTE_HOME } from "../../constants/routes";
 const Navbar = () => {
   return (
     <nav>
-      <ul className="flex space-x-10 font-light tracking-wider text-font uppercase text-sm">
+      <ul className="flex space-x-5 font-light tracking-wider text-font uppercase text-sm">
         <li>
-          <Link href={ROUTE_HOME}>Home</Link>
+          <Link
+            href={ROUTE_HOME}
+            className="hover:bg-secondary px-3 py-2 rounded-sm transition-colors"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link href={ROUTE_ABOUT}>About</Link>
+          <Link
+            href={ROUTE_ABOUT}
+            className="hover:bg-secondary px-3 py-2 rounded-sm transition-colors"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link href={ROUTE_CONTACT}>Contact</Link>
+          <Link
+            href={ROUTE_CONTACT}
+            className="hover:bg-secondary px-3 py-2 rounded-sm transition-colors"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
