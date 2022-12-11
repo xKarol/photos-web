333dff52c125f63163db73d9b18488ab07664d70
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-11 19:14:28 +0000

    Add better styles for mobile navbar component

--- frontend/src/components/header/navbar-mobile.tsx
+++ frontend/src/components/header/navbar-mobile.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { ROUTE_ABOUT, ROUTE_CONTACT, ROUTE_HOME } from "../../constants/routes";
+import { HiX } from "react-icons/hi";
 
 type Props = {
   onClose: () => void;
@@ -9,8 +10,8 @@ type Props = {
 const NavbarMobile = ({ onClose }: Props) => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 bg-white flex justify-center z-10">
-      <nav className="mt-10">
-        <ul className="flex flex-col space-y-5 font-light tracking-wider items-center">
+      <nav className="mt-20">
+        <ul className="flex flex-col space-y-5 text-xl font-light [&_>*]:uppercase text-font tracking-wider items-center">
           <li>
             <Link href={ROUTE_HOME}>Home</Link>
           </li>
@@ -22,9 +23,10 @@ const NavbarMobile = ({ onClose }: Props) => {
           </li>
         </ul>
       </nav>
-      <div className="fixed right-5 top-5 cursor-pointer" onClick={onClose}>
-        X
-      </div>
+      <HiX
+        className="fixed right-5 top-5 cursor-pointer text-2xl text-font"
+        onClick={onClose}
+      />
     </div>
   );
 };
