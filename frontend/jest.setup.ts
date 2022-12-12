db20e369be4809b28158bbdeb4ff1c227bf130f9
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-12 12:49:50 +0000

    Add msw

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,5 +1,11 @@
 import "@testing-library/jest-dom/extend-expect";
 
+import { server } from "./src/__mocks__/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
