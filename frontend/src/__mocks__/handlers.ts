56148b4966609ec32706c7b2cc3204f37dc308f6
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-12 15:19:13 +0000

    Add /contact route mock

--- frontend/src/__mocks__/handlers.ts
+++ frontend/src/__mocks__/handlers.ts
@@ -4,4 +4,7 @@ export const handlers = [
   rest.post("/newsletter/subscribe", (_req, res, ctx) => {
     return res(ctx.status(200));
   }),
+  rest.post("/contact", (_req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
 ];
