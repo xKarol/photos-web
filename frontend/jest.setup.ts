6b3e5c9125c3d74e8c058800837153ef6e149434
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-13 13:25:56 +0000

    Clear query cache after each test

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,9 +1,14 @@
 import "@testing-library/jest-dom/extend-expect";
 
 import { server } from "./src/__mocks__/server";
+import { QueryCache } from 'react-query'
+const queryCache = new QueryCache()
 
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  queryCache.clear();
+});
 afterAll(() => server.close());
 
 Object.defineProperty(window, "matchMedia", {
