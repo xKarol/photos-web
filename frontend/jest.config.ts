eb3b44b10bad1314bda98b1e689cf009216cc1c5
Karol <83913433+xKarol@users.noreply.github.com>
2022-12-14 16:41:28 +0000

    Add types to jest config

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,10 +1,11 @@
 import nextJest from "next/jest";
+import type { Config } from "@jest/types";
 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
-const customJestConfig = {
+const customJestConfig: Config.InitialOptions = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
 };
