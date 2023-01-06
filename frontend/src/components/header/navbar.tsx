164d83558cb4531de6d3705176bedc94f9d08cf9
Karol <83913433+xKarol@users.noreply.github.com>
2023-01-06 19:09:09 +0000

    Add portfolio page

--- frontend/src/components/header/navbar.tsx
+++ frontend/src/components/header/navbar.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
-import { ROUTE_ABOUT, ROUTE_CONTACT, ROUTE_HOME } from "../../constants/routes";
+import {
+  ROUTE_ABOUT,
+  ROUTE_CONTACT,
+  ROUTE_HOME,
+  ROUTE_PORTFOLIO,
+} from "../../constants/routes";
 
 const Navbar = () => {
   return (
@@ -12,6 +17,14 @@ const Navbar = () => {
             className="hover:bg-secondary px-3 py-2 rounded-sm transition-colors"
           >
             Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={ROUTE_PORTFOLIO}
+            className="hover:bg-secondary px-3 py-2 rounded-sm transition-colors"
+          >
+            Portfolio
           </Link>
         </li>
         <li>
--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,3 +1,4 @@
 export const ROUTE_HOME = "/";
 export const ROUTE_ABOUT = "/about";
 export const ROUTE_CONTACT = "/contact";
+export const ROUTE_PORTFOLIO = "/portfolio";
