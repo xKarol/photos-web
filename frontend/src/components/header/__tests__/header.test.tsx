7b8472d23435ba6f71c5c5716523749326aa48d9
Karol <83913433+xKarol@users.noreply.github.com>
2023-01-09 20:06:54 +0000

    Add more header tests

--- frontend/src/components/header/__tests__/header.test.tsx
+++ frontend/src/components/header/__tests__/header.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, within } from "@testing-library/react";
 import { Header } from "../index";
+import { navbarItems } from "../navbar-items";
 
 const setup = () => render(<Header />);
 
@@ -17,11 +18,25 @@ describe("Header", () => {
     const nav = screen.getByRole("navigation");
     expect(nav).not.toBeEmptyDOMElement();
     const links = within(nav).getAllByRole("link");
-    expect(links.length).toBeGreaterThan(0);
+    expect(links.length).toBe(navbarItems.length);
+  });
 
-    for (const link of links) {
-      expect(link).toHaveAttribute("href");
-      expect(link).not.toBeEmptyDOMElement();
+  it("should contain valid content", () => {
+    setup();
+
+    const nav = screen.getByRole("navigation");
+    const links = within(nav).getAllByRole("link");
+
+    for (const [index, link] of Object.entries(links)) {
+      expect(link).toHaveAttribute("href", navbarItems[+index].href);
+      expect(link).toHaveTextContent(navbarItems[+index].text);
     }
+  });
+
+  it("hamburger should be visible", () => {
+    setup();
+
+    const hamburger = screen.queryByLabelText(/navbar/i);
+    expect(hamburger).not.toBeInTheDocument();
   });
 });
--- frontend/src/components/header/hamburger.tsx
+++ frontend/src/components/header/hamburger.tsx
@@ -7,6 +7,7 @@ const Hamburger = ({ className, ...props }: Props) => {
   return (
     <button
       className={clsx("flex flex-col space-y-1 w-[30px]", className)}
+      aria-label="Open the navbar"
       {...props}
     >
       <span className="h-[2px] w-full bg-black" />
