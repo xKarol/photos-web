f08723aa9d63b6beec03af1a448c82c2e2f86db1
Karol <83913433+xKarol@users.noreply.github.com>
2023-01-10 11:35:57 +0000

    Removed transition & bg opacity in lightbox component

--- frontend/src/components/lightbox/lightbox.tsx
+++ frontend/src/components/lightbox/lightbox.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog } from "@headlessui/react";
 import { VscClose } from "react-icons/vsc";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { PhotoType } from "../../@types/photos";
 import Photo from "../photos-grid/photo";
 import { getImageUrl } from "../../utils/misc";
+import clsx from "clsx";
 
 type Props = {
   isOpen?: boolean;
@@ -14,7 +15,8 @@ type Props = {
   onClickNext?: (currentIndex: number) => void;
   onClickPrev?: (currentIndex: number) => void;
   onClose?: () => void;
-};
+  overlayOpacity?: number;
+} & React.ComponentPropsWithoutRef<"div">;
 
 const Lightbox = ({
   isOpen = true,
@@ -24,6 +26,7 @@ const Lightbox = ({
   onClose,
   onClickNext,
   onClickPrev,
+  className,
 }: Props) => {
   const [active, setActive] = useState(initialIndex || 0);
 
@@ -48,86 +51,69 @@ const Lightbox = ({
   };
 
   return (
-    <Transition appear show={isOpen} as={React.Fragment}>
-      <Dialog as="div" className="relative z-50" onClose={() => {}}>
-        <Transition.Child
-          as={React.Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Dialog.Overlay
-            className="fixed inset-0 bg-black bg-opacity-75"
-            aria-label="dialog overlay"
-          />
-        </Transition.Child>
+    <Dialog
+      as="div"
+      className={clsx("relative z-50", className)}
+      onClose={() => {}}
+      open={isOpen}
+    >
+      <Dialog.Overlay
+        className="fixed inset-0 bg-black"
+        aria-label="dialog overlay"
+      />
 
-        <button
-          className="fixed top-5 right-5 text-white cursor-pointer z-10 text-3xl"
-          onClick={handleClose}
-          aria-label="close"
-        >
-          <VscClose />
-        </button>
+      <button
+        className="fixed top-5 right-5 text-white cursor-pointer z-10 text-3xl"
+        onClick={handleClose}
+        aria-label="close"
+      >
+        <VscClose />
+      </button>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="fixed left-0 h-full w-[50vw] text-3xl text-white z-10">
-            {!isFirst && (
-              <button
-                className="fixed left-5 top-1/2 -translate-y-1/2 cursor-pointer"
-                onClick={() => changePhoto("prev")}
-                aria-label="previous photo"
-              >
-                <HiChevronLeft />
-              </button>
-            )}
-          </div>
-          <div className="fixed right-0 h-full w-[50vw] text-3xl text-white z-10">
-            {!isLast && (
-              <button
-                className="fixed right-5 top-1/2 -translate-y-1/2 cursor-pointer"
-                onClick={() => changePhoto("next")}
-                aria-label="next photo"
-              >
-                <HiChevronRight />
-              </button>
-            )}
-          </div>
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={React.Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="fixed left-0 h-full w-[50vw] text-3xl text-white z-10">
+          {!isFirst && (
+            <button
+              className="fixed left-5 top-1/2 -translate-y-1/2 cursor-pointer"
+              onClick={() => changePhoto("prev")}
+              aria-label="previous photo"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden transition-all">
-                {!isEmpty && (
-                  <Photo
-                    src={getImageUrl(id)}
-                    alt={alt}
-                    width={width}
-                    height={height}
-                    blurDataURL={placeholder}
-                    style={{
-                      width: "100%",
-                      height: "auto",
-                      maxHeight: "80vh",
-                      objectFit: "contain",
-                    }}
-                  />
-                )}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+              <HiChevronLeft />
+            </button>
+          )}
         </div>
-      </Dialog>
-    </Transition>
+        <div className="fixed right-0 h-full w-[50vw] text-3xl text-white z-10">
+          {!isLast && (
+            <button
+              className="fixed right-5 top-1/2 -translate-y-1/2 cursor-pointer"
+              onClick={() => changePhoto("next")}
+              aria-label="next photo"
+            >
+              <HiChevronRight />
+            </button>
+          )}
+        </div>
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <Dialog.Panel className="w-full max-w-md transform overflow-hidden transition-all">
+            {!isEmpty && (
+              <Photo
+                src={getImageUrl(id)}
+                alt={alt}
+                width={width}
+                height={height}
+                blurDataURL={placeholder}
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  maxHeight: "80vh",
+                  objectFit: "contain",
+                }}
+              />
+            )}
+          </Dialog.Panel>
+        </div>
+      </div>
+    </Dialog>
   );
 };
 
