1d8ec0c14dfb815ae1fc10bc3c6ff844f05bae9d
Karol <83913433+xKarol@users.noreply.github.com>
2023-01-16 16:41:49 +0000

    Change portfolios arrow functions to normal function

--- apps/web/src/services/portfolios.ts
+++ apps/web/src/services/portfolios.ts
@@ -3,20 +3,19 @@ import type { API } from "types";
 
 const SERVER_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-// TODO change arrow func to default function
-export const getPortfolios = async (
+export async function getPortfolios(
   page = 1,
   limit = 10
-): Promise<API["Portfolios"]["Get"]> => {
+): Promise<API["Portfolios"]["Get"]> {
   const { data } = await axios.get(
     `${SERVER_URL}/portfolios?page=${page}&limit=${limit}`
   );
   return data;
-};
+}
 
-export const getPortfolio = async (
+export async function getPortfolio(
   slug: string
-): Promise<API["Portfolios"]["GetOne"]> => {
+): Promise<API["Portfolios"]["GetOne"]> {
   const { data } = await axios.get(`${SERVER_URL}/portfolios/${slug}`);
   return data;
-};
+}
