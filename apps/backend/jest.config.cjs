0fe207b1076615da04529e94d2d578ce5e16eefe
Karol <83913433+xKarol@users.noreply.github.com>
2023-01-19 10:16:13 +0000

    Add .env.test file

--- apps/backend/jest.config.cjs
+++ apps/backend/jest.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  setupFiles: ["<rootDir>/jest.setup"],
 };
