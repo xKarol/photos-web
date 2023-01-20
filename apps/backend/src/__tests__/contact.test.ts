e5a515cba7b29b02b72c26400efea57e1f8f65cc
Karol <83913433+xKarol@users.noreply.github.com>
2023-01-20 13:33:03 +0000

    Add contact test

---
+++ apps/backend/src/__tests__/contact.test.ts
@@ -0,0 +1,46 @@
+import supertest from "supertest";
+import app from "../app";
+import { faker } from "@faker-js/faker";
+import { prisma } from "../db";
+
+const request = supertest(app);
+
+describe("Contact", () => {
+  beforeAll(async () => {
+    prisma.contact.deleteMany({});
+  });
+  let contactId: string;
+
+  describe("POST /contact", () => {
+    it("should send new contact form", async () => {
+      const input = {
+        email: faker.internet.email(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.firstName(),
+        message: faker.lorem.words(50),
+        subject: faker.lorem.words(7),
+      };
+      const { body } = await request.post("/contact").send(input).expect(200);
+      contactId = body.id;
+      expect(body).not.toBeEmptyObject();
+    });
+    it("should throw error when provided input is invalid", async () => {
+      const { body } = await request.post("/contact").expect(400);
+      expect(typeof body?.message).toBe("string");
+    });
+  });
+  describe("DELETE /contact", () => {
+    it("should delete contact", async () => {
+      const { body } = await request
+        .delete(`/contact/${contactId}`)
+        .expect(200);
+      expect(body).toBeEmptyObject();
+    });
+    it("should throw 404 status when contact not exist", async () => {
+      const { body } = await request
+        .delete("/contact/invalidContactId")
+        .expect(404);
+      expect(typeof body?.message).toBe("string");
+    });
+  });
+});
