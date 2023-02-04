0f019b61e69b20deda28e7adce8ffa06aac853e7
Karol <karolpiskorz8@gmail.com>
2023-02-04 15:17:12 +0100

    Fix zindex in carousel

--- apps/web/src/components/lightbox/lightbox.tsx
+++ apps/web/src/components/lightbox/lightbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Dialog } from "@headlessui/react";
 import { VscClose } from "react-icons/vsc";
 import { TfiAngleRight, TfiAngleLeft } from "react-icons/tfi";
@@ -34,150 +34,74 @@ const Lightbox = ({
   const handleClose = () => {};
 
   return (
-    <>
-      <style jsx>
-        {`
-          .carousel .slider {
-            z-index: 1;
-          }
-        `}
-      </style>
-      <Dialog
-        as="div"
-        className={clsx("relative z-50", className)}
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        onClose={() => {}}
-        open={isOpen}
+    <Dialog
+      as="div"
+      className={clsx("relative z-50", className)}
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      onClose={() => {}}
+      open={isOpen}
+    >
+      <Dialog.Overlay
+        className="fixed inset-0 bg-black"
+        aria-label="dialog overlay"
+      />
+
+      <button
+        className="fixed top-5 right-5 text-white cursor-pointer z-10 text-3xl"
+        onClick={handleClose}
+        aria-label="close"
       >
-        <Dialog.Overlay
-          className="fixed inset-0 bg-black"
-          aria-label="dialog overlay"
-        />
-
-        <button
-          className="fixed top-5 right-5 text-white cursor-pointer z-10 text-3xl"
-          onClick={handleClose}
-          aria-label="close"
-        >
-          <VscClose />
-        </button>
-        <Carousel
-          showArrows
-          showIndicators={false}
-          animationHandler="fade"
-          className="z-50"
-          renderArrowNext={(onClickHandler, hasNext, label) =>
-            hasNext && (
-              <button
-                type="button"
-                onClick={onClickHandler}
-                title={label}
-                className="z-[2] p-5 text-white absolute right-0 top-0 bottom-0 w-1/2 text-3xl flex justify-end items-center"
-              >
-                <TfiAngleRight />
-              </button>
-            )
-          }
-          renderArrowPrev={(onClickHandler, hasPrev, label) =>
-            hasPrev && (
-              <button
-                type="button"
-                onClick={onClickHandler}
-                title={label}
-                className="z-[2] p-5 text-white absolute left-0 top-0 bottom-0 w-1/2 text-3xl flex items-center justify-start"
-              >
-                <TfiAngleLeft />
-              </button>
-            )
-          }
-        >
-          {photos.map(({ height, width, placeholder, alt, id }) => (
-            <Photo
-              key={id}
-              src={getImageUrl(id)}
-              alt={alt}
-              width={width}
-              height={height}
-              blurDataURL={placeholder}
-              style={{
-                width: "100%",
-                height: "auto",
-                maxHeight: "80vh",
-                objectFit: "contain",
-              }}
-            />
-          ))}
-        </Carousel>
-      </Dialog>
-    </>
+        <VscClose />
+      </button>
+      <Carousel
+        showArrows
+        showIndicators={false}
+        animationHandler="fade"
+        className="relative z-50"
+        renderArrowNext={(onClickHandler, hasNext, label) =>
+          hasNext && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              className="z-[2] p-5 text-white absolute right-0 top-0 bottom-0 w-1/2 text-3xl flex justify-end items-center"
+            >
+              <TfiAngleRight />
+            </button>
+          )
+        }
+        renderArrowPrev={(onClickHandler, hasPrev, label) =>
+          hasPrev && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              className="z-[2] p-5 text-white absolute left-0 top-0 bottom-0 w-1/2 text-3xl flex items-center justify-start"
+            >
+              <TfiAngleLeft />
+            </button>
+          )
+        }
+      >
+        {photos.map(({ height, width, placeholder, alt, id }) => (
+          <Photo
+            key={id}
+            src={getImageUrl(id)}
+            alt={alt}
+            width={width}
+            height={height}
+            blurDataURL={placeholder}
+            style={{
+              width: "100%",
+              height: "auto",
+              maxHeight: "80vh",
+              objectFit: "contain",
+            }}
+          />
+        ))}
+      </Carousel>
+    </Dialog>
   );
-
-  // return (
-  //   <Dialog
-  //     as="div"
-  //     className={clsx("relative z-50", className)}
-  //     // eslint-disable-next-line @typescript-eslint/no-empty-function
-  //     onClose={() => {}}
-  //     open={isOpen}
-  //   >
-  //     <Dialog.Overlay
-  //       className="fixed inset-0 bg-black"
-  //       aria-label="dialog overlay"
-  //     />
-
-  //     <button
-  //       className="fixed top-5 right-5 text-white cursor-pointer z-10 text-3xl"
-  //       onClick={handleClose}
-  //       aria-label="close"
-  //     >
-  //       <VscClose />
-  //     </button>
-
-  //     <div className="fixed inset-0 overflow-y-auto">
-  //       <div className="fixed left-0 h-full w-[50vw] text-3xl text-white z-10">
-  //         {!isFirst && (
-  //           <button
-  //             className="fixed left-5 top-1/2 -translate-y-1/2 cursor-pointer"
-  //             onClick={() => changePhoto("prev")}
-  //             aria-label="previous photo"
-  //           >
-  //             <HiChevronLeft />
-  //           </button>
-  //         )}
-  //       </div>
-  //       <div className="fixed right-0 h-full w-[50vw] text-3xl text-white z-10">
-  //         {!isLast && (
-  //           <button
-  //             className="fixed right-5 top-1/2 -translate-y-1/2 cursor-pointer"
-  //             onClick={() => changePhoto("next")}
-  //             aria-label="next photo"
-  //           >
-  //             <HiChevronRight />
-  //           </button>
-  //         )}
-  //       </div>
-  //       <div className="flex min-h-full items-center justify-center p-4 text-center">
-  //         <Dialog.Panel className="w-full max-w-md transform overflow-hidden transition-all">
-  //           {!isEmpty && (
-  //             <Photo
-  //               src={getImageUrl(id)}
-  //               alt={alt}
-  //               width={width}
-  //               height={height}
-  //               blurDataURL={placeholder}
-  //               style={{
-  //                 width: "100%",
-  //                 height: "auto",
-  //                 maxHeight: "80vh",
-  //                 objectFit: "contain",
-  //               }}
-  //             />
-  //           )}
-  //         </Dialog.Panel>
-  //       </div>
-  //     </div>
-  //   </Dialog>
-  // );
 };
 
 export default Lightbox;
