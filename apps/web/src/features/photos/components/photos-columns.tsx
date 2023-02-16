a79e8ca9b436a8778848e54e27df411d70f8b5f6
Karol <karolpiskorz8@gmail.com>
2023-02-16 18:56:26 +0100

    Better images height calculation on mobile screens

--- apps/web/src/features/photos/components/photos-columns.tsx
+++ apps/web/src/features/photos/components/photos-columns.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { useMedia } from "react-use";
+import { useMedia, useWindowSize } from "react-use";
 import type { Image as ImageType } from "types";
 import { getImageUrl } from "../../../utils/misc";
 import useImagePositions from "../hooks/use-image-position";
@@ -10,13 +10,17 @@ type Props = {
   photos?: ImageType[];
 } & React.ComponentPropsWithoutRef<"div">;
 
+const DEFAULT_ELEMENT_WIDTH = 520;
+const MOBILE_PADDING = 32;
+
 const PhotosColumns = ({ photos = [], ...props }: Props) => {
   const isMobile = useMedia("(max-width: 500px)", false);
+  const { width } = useWindowSize(DEFAULT_ELEMENT_WIDTH);
 
   const { positions, getMaxHeight } = useImagePositions(photos, {
     gap: 50,
     columns: isMobile ? 1 : 2,
-    elementWidth: 526, //TODO calc based on screen width
+    elementWidth: isMobile ? width - MOBILE_PADDING : DEFAULT_ELEMENT_WIDTH,
   });
 
   return (
