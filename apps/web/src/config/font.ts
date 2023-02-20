22d05dccdefbec78ee3386db287527730902858c
Karol <83913433+xKarol@users.noreply.github.com>
2023-02-20 11:10:55 +0000

    Moved font config to /config folder

---
+++ apps/web/src/config/font.ts
@@ -0,0 +1,25 @@
+import localFont from "@next/font/local";
+
+const defaultFont = localFont({
+  variable: "--font-montserrat",
+  src: [
+    {
+      path: "../../public/assets/fonts/montserrat/Montserrat-ExtraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "../../public/assets/fonts/montserrat/Montserrat-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "../../public/assets/fonts/montserrat/Montserrat-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/assets/fonts/montserrat/Montserrat-Medium.ttf",
+      weight: "500",
+    },
+  ],
+});
+
+export default defaultFont;
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,31 +2,9 @@ import React, { useEffect, useState } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Hydrate, QueryClientProvider, QueryClient } from "react-query";
-import localFont from "@next/font/local";
 import { DefaultSeo } from "next-seo";
 import SEO from "../config/next-seo";
-
-const defaultFont = localFont({
-  variable: "--font-montserrat",
-  src: [
-    {
-      path: "../../public/assets/fonts/montserrat/Montserrat-ExtraLight.ttf",
-      weight: "200",
-    },
-    {
-      path: "../../public/assets/fonts/montserrat/Montserrat-Light.ttf",
-      weight: "300",
-    },
-    {
-      path: "../../public/assets/fonts/montserrat/Montserrat-Regular.ttf",
-      weight: "400",
-    },
-    {
-      path: "../../public/assets/fonts/montserrat/Montserrat-Medium.ttf",
-      weight: "500",
-    },
-  ],
-});
+import defaultFont from "../config/font";
 
 const ReactQueryDevtoolsProduction = React.lazy(() =>
   import("react-query/devtools/development").then((d) => ({
