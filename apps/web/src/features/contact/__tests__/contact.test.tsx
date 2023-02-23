38fd764a4a5fa5757812f11818c1fdd26d0bf5df
Karol <83913433+xKarol@users.noreply.github.com>
2023-02-23 13:49:24 +0000

    Fix contact tests

--- apps/web/src/features/contact/__tests__/contact.test.tsx
+++ apps/web/src/features/contact/__tests__/contact.test.tsx
@@ -14,18 +14,8 @@ const setup = () =>
     </QueryClientProvider>
   );
 
-const getSubmitElement = () => screen.getByRole("button", { name: /Submit/i });
+const getSubmitElement = () => screen.getByRole("button");
 const getInputElements = () => screen.getAllByRole("textbox");
-
-const generateFakeFormData = () => {
-  return [
-    faker.name.firstName(),
-    faker.name.lastName(),
-    faker.internet.email(),
-    faker.lorem.words(10),
-    faker.lorem.words(30),
-  ];
-};
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -44,8 +34,7 @@ describe("Contact", () => {
 
   it("submit should be visible", () => {
     setup();
-    const submitElement = getSubmitElement();
-    expect(submitElement).toBeVisible();
+    expect(getSubmitElement()).toBeVisible();
   });
 
   it("errors should not be visible on initial load", () => {
@@ -66,7 +55,6 @@ describe("Contact", () => {
     setup();
 
     const { click } = userEvent.setup();
-
     const submitElement = getSubmitElement();
     await click(submitElement);
 
@@ -82,9 +70,7 @@ describe("Contact", () => {
 
     const inputElements = getInputElements();
     await type(inputElements[0], faker.name.firstName());
-
-    const submitElement = getSubmitElement();
-    await click(submitElement);
+    await click(getSubmitElement());
 
     const errors = await screen.findAllByRole("alert");
     expect(errors.length).toBe(inputElements.length - 1);
@@ -93,46 +79,27 @@ describe("Contact", () => {
   it("should reset form after OK response", async () => {
     setup();
 
-    const { type, click } = userEvent.setup();
-    const inputsData = generateFakeFormData();
+    await fillForm();
+    const { click } = userEvent.setup();
+    await click(getSubmitElement());
+
     const inputElements = getInputElements();
-
-    for await (const [index, value] of inputsData.entries()) {
-      const input = inputElements[index];
-      await type(input, value);
-      expect(input).toHaveValue(value);
-    }
-
-    const submitElement = getSubmitElement();
-    await click(submitElement);
-
     for (const input of inputElements) {
       expect(input).toHaveValue("");
     }
   });
 
   it("should display error message", async () => {
-    const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-
     server.use(
-      rest.post(`${BACKEND_URL}/contact`, (_req, res, ctx) => {
+      rest.post("/contact", (_req, res, ctx) => {
         return res(ctx.status(400), ctx.json({}));
       })
     );
 
     setup();
-
-    const { type, click } = userEvent.setup();
-    const inputsData = generateFakeFormData();
-
-    const submitElement = screen.getByRole("button", { name: /Submit/i });
-    const inputElements = screen.getAllByRole("textbox");
-
-    for await (const [index, value] of inputsData.entries()) {
-      const input = inputElements[index];
-      await type(input, value);
-    }
-    await click(submitElement);
+    await fillForm();
+    const { click } = userEvent.setup();
+    await click(getSubmitElement());
 
     const infoElement = await screen.findByRole("alert");
     expect(infoElement).toBeInTheDocument();
@@ -141,21 +108,31 @@ describe("Contact", () => {
 
   it("should display OK message", async () => {
     setup();
-
-    const { type, click } = userEvent.setup();
-    const inputsData = generateFakeFormData();
-
-    const submitElement = screen.getByRole("button", { name: /Submit/i });
-    const inputElements = screen.getAllByRole("textbox");
-
-    for await (const [index, value] of inputsData.entries()) {
-      const input = inputElements[index];
-      await type(input, value);
-    }
-    await click(submitElement);
+    await fillForm();
+    const { click } = userEvent.setup();
+    await click(getSubmitElement());
 
     const infoElement = await screen.findByRole("alert");
     expect(infoElement).toBeInTheDocument();
-    expect(infoElement).toHaveTextContent(/Message has been sent./i);
+    expect(infoElement).toHaveTextContent(/Message has been sent/i);
   });
 });
+
+async function fillForm() {
+  const formData = [
+    faker.name.firstName(),
+    faker.name.lastName(),
+    faker.internet.email(),
+    faker.lorem.words(10),
+    faker.lorem.words(30),
+  ];
+
+  const inputElements = getInputElements();
+  const { type } = userEvent.setup();
+
+  for await (const [index, value] of formData.entries()) {
+    const input = inputElements[index];
+    await type(input, value);
+    expect(input).toHaveValue(value);
+  }
+}
