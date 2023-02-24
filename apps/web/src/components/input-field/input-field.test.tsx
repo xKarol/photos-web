762eb8d823b529d758050709dad37f63133dca2f
Karol <83913433+xKarol@users.noreply.github.com>
2023-02-24 14:11:04 +0000

    Add input-field tests

---
+++ apps/web/src/components/input-field/input-field.test.tsx
@@ -0,0 +1,78 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import InputField from "./input-field";
+
+describe("InputField", () => {
+  it("should render 'input' element", () => {
+    render(<InputField />);
+    const inputEl = screen.getByRole("textbox");
+    expect(inputEl).toBeInTheDocument();
+    expect(inputEl.nodeName).toBe("INPUT");
+  });
+
+  it("'textarea' element should render when textarea was provided", () => {
+    render(<InputField textarea />);
+    const textareaEl = screen.getByRole("textbox");
+    expect(textareaEl).toBeInTheDocument();
+    expect(textareaEl.nodeName).toBe("TEXTAREA");
+  });
+
+  it("should render label text", () => {
+    render(<InputField label="test" />);
+    expect(screen.getByText(/test/i)).toBeInTheDocument();
+  });
+
+  it("base component should render without required * character", () => {
+    render(<InputField />);
+    expect(screen.queryByText("*")).not.toBeInTheDocument();
+  });
+
+  it("* should be visible when required prop is set", () => {
+    render(<InputField required />);
+    expect(screen.getByText("*")).toBeInTheDocument();
+  });
+
+  it("error message should not be visible in base component", () => {
+    render(<InputField />);
+    expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+  });
+
+  it("error message should be visible when provided", () => {
+    render(<InputField error="custom error" />);
+    expect(
+      screen.getByRole("alert", { name: /custom error/i })
+    ).toBeInTheDocument();
+  });
+
+  it("the classes of both elements should match", () => {
+    render(<InputField />);
+    const initialClasses = screen.getByRole("textbox").className;
+    cleanup();
+    render(<InputField textarea />);
+    expect(screen.getByRole("textbox").className).toMatch(initialClasses);
+  });
+
+  it("label and required character should be displayed", () => {
+    render(<InputField label="test label" required />);
+    expect(screen.getByText(/test label/i).textContent).toBe("test label *");
+  });
+
+  it("the user can type in the text box", async () => {
+    render(<InputField />);
+    const inputElement = screen.getByRole("textbox");
+    const value = "custom value";
+    const { type } = userEvent.setup();
+    await type(inputElement, value);
+    expect(inputElement).toHaveValue(value);
+  });
+
+  it("'text' should be default input type", () => {
+    render(<InputField />);
+    expect(screen.getByRole("textbox").getAttribute("type")).toBe("text");
+  });
+
+  it("input should render with custom input type", () => {
+    render(<InputField type="email" />);
+    expect(screen.getByRole("textbox").getAttribute("type")).toBe("email");
+  });
+});
--- apps/web/src/components/input-field/input-field.tsx
+++ apps/web/src/components/input-field/input-field.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useId } from "react";
+import Alert from "../alert";
 
 export type InputFieldProps = {
   label?: string;
@@ -13,30 +14,42 @@ const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
     { label, error, type = "text", className, textarea, required, ...rest },
     ref
   ) => {
+    const id = useId();
     const inputClass = "border border-zinc-300 p-2";
     return (
       <div className={clsx("relative flex w-full flex-col", className)}>
-        <label className="mb-1 text-xs">
+        <label className="mb-1 text-xs" htmlFor={id}>
           {label} {required ? <span className="text-red-500">*</span> : null}
         </label>
         {textarea ? (
           <textarea
+            id={id}
+            aria-describedby={id}
+            aria-invalid={error ? "true" : "false"}
             className={clsx("min-h-[50px]", inputClass)}
             {...rest}
-            // @ts-ignore
+            // @ts-expect-error
             ref={ref}
           />
         ) : (
-          <input type={type} className={inputClass} {...rest} ref={ref} />
+          <input
+            type={type}
+            className={inputClass}
+            {...rest}
+            ref={ref}
+            id={id}
+            aria-describedby={id}
+            aria-invalid={error ? "true" : "false"}
+          />
         )}
         {error ? (
-          <span
-            className="absolute left-0 top-[calc(100%_+_5px)] max-w-full truncate text-xs text-red-400"
-            role="alert"
+          <Alert
+            variant="error"
+            className="absolute left-0 top-[calc(100%_+_2px)] max-w-full truncate text-xs"
             title={error}
           >
             {error}
-          </span>
+          </Alert>
         ) : null}
       </div>
     );
