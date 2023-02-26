beae2ef8b251286d672956fe79cd53cebefd7501
Karol <83913433+xKarol@users.noreply.github.com>
2023-02-26 15:29:44 +0000

    Changes in auth utils

--- apps/backend/src/utils/auth.ts
+++ apps/backend/src/utils/auth.ts
@@ -3,34 +3,29 @@ import { Strategy as LocalStrategy } from "passport-local";
 import type { VerifyFunction } from "passport-local";
 import type { Express } from "express";
 import session from "express-session";
-import crypto from "node:crypto";
 
 const sessionConfig = {
   secret: process.env.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: true,
   cookie: { maxAge: 60 * 3 * 1000 },
-  genid: () => crypto.randomUUID(),
 };
 
+type User = { username: string };
+
 const initAuth = (app: Express) => {
-  app.use(passport.initialize());
-  app.use(session(sessionConfig));
   passport.use(new LocalStrategy((...args) => verifyAdmin(args)));
 
   passport.serializeUser(function (user, cb) {
-    process.nextTick(function () {
-      console.log("serialize", user);
-      cb(null, user);
-    });
+    cb(null, (user as User).username);
   });
 
   passport.deserializeUser(function (user, cb) {
-    process.nextTick(function () {
-      console.log("deserialize", user);
-      return cb(null, user as { id: string });
-    });
+    return cb(null, (user as User).username);
   });
+
+  app.use(passport.initialize());
+  app.use(session(sessionConfig));
 };
 
 export default initAuth;
@@ -40,7 +35,7 @@ function verifyAdmin([username, password, done]: Parameters<VerifyFunction>) {
     process.env.ADMIN_LOGIN === username &&
     process.env.ADMIN_PASSWORD === password
   ) {
-    return done(null, { id: crypto.randomUUID() });
+    return done(null, { username });
   }
   return done("Invalid credentials.", false);
 }
