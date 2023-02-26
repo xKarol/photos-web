416a82cae558d56831acfa11cd3651e320f996ef
Karol <83913433+xKarol@users.noreply.github.com>
2023-02-26 16:57:15 +0000

    Add basic portfolios test

--- apps/web/src/__mocks__/handlers.ts
+++ apps/web/src/__mocks__/handlers.ts
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+import type { Portfolios } from "types";
 
 export const handlers = [
   rest.post(`/newsletter/subscribe`, (_req, res, ctx) => {
@@ -6,5 +7,26 @@ export const handlers = [
   }),
   rest.post(`/contact`, (_req, res, ctx) => {
     return res(ctx.status(200));
+  }),
+  rest.get(`/portfolios`, (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json<Portfolios[]>([
+        {
+          id: "12122",
+          name: "test",
+          slug: "test",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: "122122",
+          name: "test1",
+          slug: "test1",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ])
+    );
   }),
 ];
---
+++ apps/web/src/features/portfolios/__tests__/portfolios.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { rest } from "msw";
+// eslint-disable-next-line jest/no-mocks-import
+import { server } from "../../../__mocks__/server";
+import Portfolios from "../portfolios";
+
+const setup = () =>
+  render(
+    <QueryClientProvider client={queryClient}>
+      <Portfolios />
+    </QueryClientProvider>
+  );
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+describe("Portfolios", () => {
+  it("should render empty state component", () => {
+    server.use(
+      rest.post("/portfolios", (_req, res, ctx) => {
+        return res(
+          ctx.status(400),
+          ctx.json({ status: 400, message: "Test Error Message" })
+        );
+      })
+    );
+    setup();
+    expect(screen.getByLabelText(/animation/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /no portfolios found/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+  it("should render portfolios list", () => {
+    // TODO create more tests...
+  });
+});
