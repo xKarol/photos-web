3b8e2821f89361b1b8c484d6e639d203ca16bc8b
Karol <83913433+xKarol@users.noreply.github.com>
2023-02-26 16:21:46 +0000

    Minor change in auth router

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import passport from "passport";
 import * as auth from "../controllers/auth";
 
 const router = express.Router();
 
-router.post("/admin/login", auth.Login);
+// router.post("/admin/login", auth.Login);
+
+router.post("/admin/login", passport.authenticate("local", {}), (req, res) => {
+  res.sendStatus(200);
+});
 
 router.post("/admin/logout", auth.Logout);
 
