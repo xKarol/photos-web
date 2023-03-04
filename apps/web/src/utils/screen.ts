9885e9b54c65bf5d1faa60b5549ea62a4d7a45c0
Karol <karolpiskorz8@gmail.com>
2023-03-04 15:43:16 +0100

    Removed unused code

--- apps/web/src/utils/screen.ts
+++ apps/web/src/utils/screen.ts
@@ -1,8 +1,6 @@
 import defaultConfig from "tailwindcss/defaultConfig";
-import customConfig from "../../tailwind.config";
 
 const defaultScreens = defaultConfig.theme.screens;
-const containerPadding = customConfig.theme.extend.container.padding;
 
 export const getScreenSizes = ({
   asNumbers = false,
@@ -15,30 +13,6 @@ export const getScreenSizes = ({
       number
     >;
   return defaultScreens as Record<"sm" | "md" | "lg" | "xl" | "2xl", string>;
-};
-
-export const getScreenName = (screenWidth: number) => {
-  const screensValues = getScreenSizes({
-    asNumbers: true,
-  }) as unknown as Record<"sm" | "md" | "lg" | "xl" | "2xl", number>; //TODO fix return types
-  for (const [key, value] of Object.entries(screensValues).reverse()) {
-    if (screenWidth >= value) return key;
-  }
-  return Object.keys(screensValues)[0];
-};
-
-export const getContainerPaddingValues = () => {
-  const paddingValues = transformObjectValuesToNumbers(containerPadding);
-  return paddingValues;
-};
-
-export const calculateContainerPadding = (screenWidth: number) => {
-  const screensValues = getScreenSizes({ asNumbers: true });
-  const screenName = getScreenName(screenWidth);
-  const paddingValues = getContainerPaddingValues();
-  if (screenWidth < screensValues[screenName])
-    return paddingValues["DEFAULT"] * 2;
-  return paddingValues[screenName] * 2;
 };
 
 function transformObjectValuesToNumbers<T>(screens: T, replaceString = "px") {
