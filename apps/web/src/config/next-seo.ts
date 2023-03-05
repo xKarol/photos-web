7ad0fc6e88c588ac202f3fe04b326a4343246246
Karol <karolpiskorz8@gmail.com>
2023-03-05 16:02:02 +0100

    Set default theme color

--- apps/web/src/config/next-seo.ts
+++ apps/web/src/config/next-seo.ts
@@ -3,6 +3,7 @@ import type { DefaultSeoProps } from "next-seo";
 const config: DefaultSeoProps = {
   defaultTitle: "Photos Web",
   titleTemplate: "Photos Web — %s",
+  themeColor: "#EDE3DA",
   description:
     "Explore captivating photos that showcase the beauty of the world around us. Our page features stunning images that capture the essence of people, places and nature. Browse our extensive collection and discover the beauty in every image.",
 };
