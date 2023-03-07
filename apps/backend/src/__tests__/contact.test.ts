807f0850d9722431dd67f4704b11168a3c2b58c5
Karol <karolpiskorz8@gmail.com>
2023-03-07 19:51:45 +0100

    Add more contact tests

--- apps/backend/src/__tests__/contact.test.ts
+++ apps/backend/src/__tests__/contact.test.ts
@@ -4,47 +4,84 @@ import type * as Schema from "../schemas/contact";
 import app from "../app";
 import { prisma } from "../lib/prisma";
 import "../mocks/auth";
+import { createContact } from "../services/contact";
+import { parseError } from "./utils";
 
 jest.mock("../middlewares/require-auth");
 
 const request = supertest(app);
 
 describe("Contact", () => {
-  beforeAll(async () => {
+  afterAll(async () => {
     prisma.contact.deleteMany({});
   });
-  let contactId: string;
 
   describe("POST /contact", () => {
     it("should create new contact form", async () => {
-      const input: Schema.CreateContact["body"] = {
-        email: faker.internet.email(),
-        firstName: faker.name.firstName(),
-        lastName: faker.name.firstName(),
-        message: faker.lorem.words(50),
-        subject: faker.lorem.words(7),
-      };
-      const { body } = await request.post("/contact").send(input).expect(200);
-      contactId = body.id;
+      const { body } = await request
+        .post("/contact")
+        .send(createFakeContactData())
+        .expect(200);
       expect(body).not.toBeEmptyObject();
     });
+
     it("should throw error when provided input is invalid", async () => {
       const { body } = await request.post("/contact").expect(400);
-      expect(typeof body?.message).toBe("string");
+      expect(parseError(body)).toBe(true);
+    });
+
+    it("should throw error when additional field was invalid", async () => {
+      const { body } = await request
+        .post("/contact")
+        .send({ ...createFakeContactData(), invalid: "field" })
+        .expect(400);
+      expect(parseError(body)).toBe(true);
+    });
+
+    it("should throw error when only one field was provided", async () => {
+      const { body } = await request
+        .post("/contact")
+        .send({ email: "test@gmail.com" })
+        .expect(400);
+      expect(parseError(body)).toBe(true);
+    });
+
+    it("should throw error when invalid field was provided", async () => {
+      const { body } = await request
+        .post("/contact")
+        .send({ invalid: "field" })
+        .expect(400);
+      expect(parseError(body)).toBe(true);
     });
   });
+
   describe("DELETE /contact", () => {
     it("should delete contact", async () => {
-      const { body } = await request
-        .delete(`/contact/${contactId}`)
-        .expect(200);
+      const { id } = await createContactRecord();
+      const { body } = await request.delete(`/contact/${id}`).expect(200);
       expect(body).toBeEmptyObject();
     });
+
     it("should throw 404 status when contact not exist", async () => {
       const { body } = await request
         .delete("/contact/invalidContactId")
         .expect(404);
-      expect(typeof body?.message).toBe("string");
+      expect(parseError(body)).toBe(true);
     });
   });
 });
+
+function createFakeContactData() {
+  const input: Schema.CreateContact["body"] = {
+    email: faker.internet.email(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.firstName(),
+    message: faker.lorem.words(50),
+    subject: faker.lorem.words(7),
+  };
+  return input;
+}
+
+async function createContactRecord() {
+  return await createContact(createFakeContactData());
+}
