e766229a63f7bcb18b1048f01c0728b0b66d8ef1
Karol <karolpiskorz8@gmail.com>
2023-03-12 20:19:25 +0100

    Change loading blur in image component

--- apps/web/src/components/photo/photo.tsx
+++ apps/web/src/components/photo/photo.tsx
@@ -1,5 +1,5 @@
 import type { ImageProps } from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { getImageUrl } from "../../utils/misc";
 import { getImagePlaceholder } from "../../utils/placeholder";
@@ -8,14 +8,26 @@ type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 type Props = { id: string } & PartialBy<ImageProps, "src">;
 
 const Photo = ({ id, alt, ...props }: Props) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { width = "100%", height = "100%" } = props;
   return (
-    <Image
-      src={getImageUrl(id)}
-      alt={alt}
-      placeholder="blur"
-      blurDataURL={getImagePlaceholder(id)}
-      {...props}
-    />
+    <div
+      className="relative"
+      style={{
+        width,
+        height,
+        ...(!isLoaded && {
+          backgroundImage: `url("${getImagePlaceholder(id)}")`,
+        }),
+      }}
+    >
+      <Image
+        src={getImageUrl(id)}
+        alt={alt}
+        {...props}
+        onLoadingComplete={() => setIsLoaded(true)}
+      />
+    </div>
   );
 };
 
