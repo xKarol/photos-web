1df7d839a0a6ce35fbad42bc460069b14918755f
Karol <karolpiskorz8@gmail.com>
2023-03-13 16:38:27 +0100

    Ignore jest utils file from tests

--- apps/backend/jest.config.cjs
+++ apps/backend/jest.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/dist/",
+    "<rootDir>/src/__tests__/utils.ts",
+  ],
   setupFilesAfterEnv: ["jest-extended/all", "<rootDir>/jest.setup.ts"],
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
 };
