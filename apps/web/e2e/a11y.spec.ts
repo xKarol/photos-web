f559e86697a7d95ce10ed2e05fe58dba7c35a7e7
Karol <karolpiskorz8@gmail.com>
2023-03-21 14:02:28 +0100

    Attaching a11y e2e test info to report

--- apps/web/e2e/a11y.spec.ts
+++ apps/web/e2e/a11y.spec.ts
@@ -7,9 +7,17 @@ test.describe("Accessibility", () => {
   for (const pagePath of pages) {
     test(`Page ${pagePath} should pass accessibility test`, async ({
       page,
-    }) => {
+    }, testInfo) => {
       await page.goto(pagePath);
       const results = await new AxeBuilder({ page }).analyze();
+      await testInfo.attach("accessibility-violations", {
+        body: JSON.stringify(results.violations, null, 2),
+        contentType: "application/json",
+      });
+      await testInfo.attach("accessibility-scan-results", {
+        body: JSON.stringify(results, null, 2),
+        contentType: "application/json",
+      });
       expect(results.violations.length).toBe(0);
     });
   }
