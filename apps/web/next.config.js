cdb79305d0f68b58d26ee182b5d60a2dd8066edb
Karol <83913433+xKarol@users.noreply.github.com>
2023-04-05 10:36:57 +0000

    Ignoring nextjs eslint errors during builds

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
