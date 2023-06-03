6a209159e67a214ea1ea19db40711956261255a6
Karol <karolpiskorz8@gmail.com>
2023-06-03 17:36:00 +0200

    Fix url parsing in cors

--- apps/backend/src/config/cors.ts
+++ apps/backend/src/config/cors.ts
@@ -1,13 +1,16 @@
 import type { CorsOptions } from "cors";
 
-const whiteList = new Set([process.env.HOST, process.env.HOST_FRONTEND]);
+const whiteList = new Set([
+  new URL(process.env.HOST as string).origin,
+  new URL(process.env.HOST_FRONTEND as string).origin,
+]);
 
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
     if (!origin || whiteList.has(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error(`Not allowed by CORS: ${origin}`));
     }
   },
 };
