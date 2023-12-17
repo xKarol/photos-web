5d8a71db1d4f2607d222ac1ce86f22d9f7d2eed8
Karol Piskorz <karolpiskorz8@gmail.com>
2023-12-17 13:38:07 +0100

    Mock ResizeObserver

--- apps/web/src/__mocks__/intersection-observer.ts
+++ apps/web/src/__mocks__/intersection-observer.ts
@@ -1,3 +1,9 @@
 import { setupIntersectionMocking } from "react-intersection-observer/test-utils";
 
 setupIntersectionMocking(jest.fn);
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
